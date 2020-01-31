b29063193a297c4c1c92e236de97f2c269771aa6
Kaan Çakmak <skaancakmak@gmail.com>
2020-01-31 03:21:07 +0300

    Update and rename hadibakalimkore.ino to Bluetooth Kontrol Modülü Kodu.ino

--- Bluetooth Kontrol Modülü Kodu.ino
+++ Bluetooth Kontrol Modülü Kodu.ino
@@ -1,6 +1,10 @@
-/*  Kod Yazarı:Süleyman Kaan Çakmak 
- *  Proje yapım Tarihi:2019-2020
- *  
+/*  Proje Adı: Discovery
+ *  Kategori: İnovasyon
+ *  Kod Yazarı:Süleyman Kaan Çakmak 
+ *  Proje yapım :2019-2020 Alanya
+ *  Proje Sahipleri:Uğur Ata Gök, Süleyman Kaan Çakmak
+ *  Proje Danışmanı: İsmet Çüçen
+ *  Okul: TED Alanya Koleji
 ██████╗░██╗░██████╗░█████╗░░█████╗░██╗░░░██╗███████╗██████╗░██╗░░░██╗
 ██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██║░░░██║██╔════╝██╔══██╗╚██╗░██╔╝
 ██║░░██║██║╚█████╗░██║░░╚═╝██║░░██║╚██╗░██╔╝█████╗░░██████╔╝░╚████╔╝░
@@ -14,7 +18,7 @@
 ██╔══██╗██║░░░░░██║░░░██║██╔══╝░░░░░██║░░░██║░░██║██║░░██║░░░██║░░░██╔══██║
 ██████╦╝███████╗╚██████╔╝███████╗░░░██║░░░╚█████╔╝╚█████╔╝░░░██║░░░██║░░██║
 ╚═════╝░╚══════╝░╚═════╝░╚══════╝░░░╚═╝░░░░╚════╝░░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝
-                                      ██
+                        ██
 ██╗░░██╗███████╗░██████╗██╗███████╗  ██████╗░░█████╗░██████╗░░█████╗░████████╗██╗░░░██╗
 ██║░██╔╝██╔════╝██╔════╝██║██╔════╝  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║░░░██║
 █████═╝░█████╗░░╚█████╗░██║█████╗░░  ██████╔╝██║░░██║██████╦╝██║░░██║░░░██║░░░██║░░░██║
@@ -22,100 +26,97 @@
 ██║░╚██╗███████╗██████╔╝██║██║░░░░░  ██║░░██║╚█████╔╝██████╦╝╚█████╔╝░░░██║░░░╚██████╔╝
 ╚═╝░░╚═╝╚══════╝╚═██══╝░╚═╝╚═╝░░░░░  ╚═╝░░╚═╝░╚════╝░╚═════╝░░╚════╝░░░░╚═╝░░░░╚═════╝░
  */
-const int motorA1  = 2;  
-const int motorB1  = 3; 
-int i = 0; 
-int a;
-int j = 0; 
-int state; 
+const int motorA1  = 2;  //Sol Motoru Burada tanımlıyoruz
+const int motorB1  = 3;  //Sağ Motoru Burada tanımlıyoruz
+int i = 0; //Herhangi bir değişken
+int a;//Herhangi bir değişken
+int j = 0; //Herhangi bir değişken
+int state; //Durumu belirtir
 int vSpeed = 255;
 void setup() {
-  pinMode(motorA1, OUTPUT);
-  pinMode(motorB1, OUTPUT);
-  pinMode(12, OUTPUT); 
-  pinMode(7, OUTPUT); 
-  pinMode(6, OUTPUT);
-  Serial.begin(9600);
+  pinMode(motorA1, OUTPUT);//Sol Motorun Pin durumunu burada atıyoruz.
+  pinMode(motorB1, OUTPUT);//Sağ Motorun Pin durumunu burada atıyoruz.
+  pinMode(12, OUTPUT);//Buzzerın Pin durumunu burada atıyoruz. 
+  pinMode(7, OUTPUT); //Sol Ledin Pin durumunu burada atıyoruz.
+  pinMode(6, OUTPUT);//Sağ Ledin Pin durumunu burada atıyoruz.
+  Serial.begin(9600);//Seri haberleşme portunun baud rateini burada ayarlıyoruz
 }
 
 void loop() {
-  /*Bluetooth bağlantısı koptuğunda veya kesildiğinde arabayı durdur.
- (Aktif etmek için alt satırın "//" larını kaldırın.)*/
-//     if(digitalRead(BTState)==LOW) { state='S'; }
-
   //Gelen veriyi 'state' değişkenine kaydet
     if(Serial.available() > 0){     
       state = Serial.read();   
     }
-  
-  /* Uygulamadan ayarlanabilen 4 hız seviyesi.(Değerler 0-255 arasında olmalı)*/
     if (state == '0'){
       vSpeed=0;}
+  
     else if (state == '1'){
       vSpeed=100;}
+  
     else if (state == '2'){
       vSpeed=180;}
+  
     else if (state == '3'){
       vSpeed=200;}
+  
     else if (state == '4'){
       vSpeed=255;}
-     
-  /***********************İleri****************************/
-  //Gelen veri 'F' ise araba ileri gider.
+  /*İleri
+  Gelen veri 'F' ise araba ileri gider.*/
     if (state == 'F') {
       analogWrite(motorA1, vSpeed); 
         analogWrite(motorB1, vSpeed);    
     }
-  /**********************İleri Sol************************/
-  //Gelen veri 'G' ise araba ileri sol(çapraz) gider.
+  /*İleri Sol
+  Gelen veri 'G' ise araba ileri sol(çapraz) gider.*/
     else if (state == 'G') {
       analogWrite(motorA1,vSpeed ); 
         analogWrite(motorB1, 100);   
     }
-  /**********************İleri Sağ************************/
-  //Gelen veri 'I' ise araba ileri sağ(çapraz) gider.
+  /**İleri Sağ
+  Gelen veri 'I' ise araba ileri sağ(çapraz) gider.*/
     else if (state == 'I') {
         analogWrite(motorA1, 100);  
         analogWrite(motorB1, vSpeed);      
     }
-  /***********************Geri****************************/
-  //Gelen veri 'B' ise araba geri gider.
+  /*Geri
+  Gelen veri 'B' ise araba geri gider.*/
     else if (state == 'B') {
       analogWrite(motorA1, 0);   
         analogWrite(motorB1, 0);   
     }
-  /**********************Geri Sol************************/
-  //Gelen veri 'H' ise araba geri sol(çapraz) gider
+  /*Geri Sol
+  Gelen veri 'H' ise araba geri sol(çapraz) gider*/
     else if (state == 'H') {
       analogWrite(motorA1, 0);  
         analogWrite(motorB1, 0);
     }
-  /**********************Geri Sağ************************/
-  //Gelen veri 'J' ise araba geri sağ(çapraz) gider
+  /*Geri Sağ
+  Gelen veri 'J' ise araba geri sağ(çapraz) gider*/
     else if (state == 'J') {
       analogWrite(motorA1, 0);   
         analogWrite(motorB1, 0);   
     }
-  /***************************Sol*****************************/
-  //Gelen veri 'L' ise araba sola gider.
+  /*Sol
+  Gelen veri 'L' ise araba sola gider.*/
     else if (state == 'L') {
       analogWrite(motorA1, vSpeed);    
         analogWrite(motorB1, 0);
     }
-  /***************************Sağ*****************************/
-  //Gelen veri 'R' ise araba sağa gider
+  /*Sağ
+  Gelen veri 'R' ise araba sağa gider*/
     else if (state == 'R') {
       analogWrite(motorA1, 0);   
         analogWrite(motorB1, vSpeed);        
     }
   
-  /************************Stop*****************************/
-  //Gelen veri 'S' ise arabayı durdur.
+  /*Dur
+  Gelen veri 'S' ise arabayı durdurur.*/
     else if (state == 'S'){
         analogWrite(motorA1, 0);  
         analogWrite(motorB1, 0);  
     }
-//Gelen veri 'W' ise arabanın on farları yanar. 
+//Gelen veri 'W' ise arabanın ön farları yanar. 
 else if (a == 'W') {
 digitalWrite(6 , HIGH);
 }
@@ -140,5 +141,4 @@ else if (a == 'u') {
 digitalWrite(7 , LOW );
 
 }
-
 }
